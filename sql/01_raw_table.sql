e6ac29f4ee8c36806362f79436aa71a2e606219f
Prabhnoor Kaur <53931569+Prabhnoor007-gr@users.noreply.github.com>
2026-02-25 14:01:06 -0800

    Update 01_raw_table.sql

--- sql/01_raw_table.sql
+++ sql/01_raw_table.sql
@@ -1,1 +1,12 @@
+-- Create raw transactional table --
 
+CREATE TABLE raw_retail (
+    InvoiceNo VARCHAR(20),
+    StockCode VARCHAR(20),
+    Description TEXT,
+    Quantity INT,
+    InvoiceDate TIMESTAMP,
+    UnitPrice DECIMAL(10,2),
+    CustomerID VARCHAR(20),
+    Country VARCHAR(50)
+);
